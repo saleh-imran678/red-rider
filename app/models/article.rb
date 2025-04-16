6271db5f8f4d8c27e1df4c2c62882008c026ff54
saleh-imran678 <Saleh.code1212@gmail.com>
2025-04-16 18:11:19 +0500

    articles module added.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :content, :author, :category, :published_at, presence: true
+
+  def as_api_json
+    as_json(only: [:id, :title, :content, :author, :category, :published_at])
+  end
 end
